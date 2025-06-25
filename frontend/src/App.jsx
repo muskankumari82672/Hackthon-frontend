49121b0c7dc6e9ea52bf5eec961eaba9c31bf1a0
muskanlumari <muskankumari82672@gmail.com>
2025-06-25 13:42:34 +0530

    ee

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     <>
       <div className="flex items-center justify-center h-screen bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
+        <h2 className="text-2xl font-bold text-red-600">
+          WELCOME TO THE JUNGLE
+        </h2>
       </div>
     </>
   );
